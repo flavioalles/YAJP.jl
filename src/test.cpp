d94bd4fa17617c21aab8749aa77d3c6b723fc621
Flavio Alles <flavio.alles@gmail.com>
2015-03-09 17:17:39 -0300

    Updating test source.

--- src/test.cpp
+++ src/test.cpp
@@ -4,9 +4,20 @@
 #include <iostream>
 #include <string>
 #include <vector>
-#include "PajeContainer.h"
-#include "PajeUnity.h"
-#include "MiscUtils.h"
+#include <PajeType.h>
+#include <PajeUnity.h>
+
+namespace paje {
+    PajeTypeNature natureTypes[] = {
+        PAJE_EventType,
+        PAJE_LinkType,
+        PAJE_StateType,
+        PAJE_VariableType,
+        PAJE_UndefinedType
+    };
+}
+
+bool dumpEntitiesSizeMap(std::map<PajeTypeNature,int> entMap);
 
 int main(int argc, char *argv[]) {
     // #arguments ok?
@@ -21,28 +32,49 @@ int main(int argc, char *argv[]) {
     }
     // instatiate PajeUnity
     PajeUnity *pajeTrace = new PajeUnity(false, false, argv[1], -1, 0);
-    // play with PajeComponent
-    // get containers
-    std::vector<PajeContainer*> containers = pajeTrace->enumeratorOfContainersInContainer(pajeTrace->rootInstance());
-    // iterate over them
-    std::vector<PajeContainer*>::iterator containersIt;
-    for (containersIt = containers.begin(); containersIt != containers.end(); containersIt++) {
-        std::vector<std::string> containerDescription = MiscUtils::splitString((*containersIt)->description(), ',');
-        std::vector<std::string>::iterator contDescriptionIt;
-        for (contDescriptionIt = containerDescription.begin(); contDescriptionIt != containerDescription.end(); contDescriptionIt++)
-            std::cout << *contDescriptionIt << " ";
-        std::cout << std::endl;
-        // iterate over container's children
-        std::vector<PajeContainer*> children = (*containersIt)->getChildren();
-        std::vector<PajeContainer*>::iterator childrenIt;
-        for (childrenIt = children.begin(); childrenIt != children.end(); childrenIt++) {
-            std::vector<std::string> childDescription = MiscUtils::splitString((*childrenIt)->description(), ',');
-            std::vector<std::string>::iterator childDescriptionIt; 
-            for (childDescriptionIt = childDescription.begin(); childDescriptionIt != childDescription.end(); childDescriptionIt++)
-                std::cout << *childDescriptionIt << " ";
-            std::cout << std::endl;
-        }
-        std::cout << std::endl;
+    double start = pajeTrace->startTime();
+    double end = pajeTrace->endTime();
+    // root
+    PajeContainer *rootContainer = pajeTrace->rootInstance();
+    std::cout << rootContainer->description() << std::endl;
+    // get containers contained by root
+    std::vector<PajeContainer*> containers = pajeTrace->enumeratorOfContainersInContainer(rootContainer);
+    // get entities by nature
+    std::vector<PajeEntity*> ents;
+    // map number of entities per PajeTypeNature
+    std::map<PajeTypeNature,int> entMap; // map number of entities per PajeTypeNature
+    for (auto& nature: paje::natureTypes) {
+        ents = rootContainer->getEntitiesOfNature(nature);
+        entMap[nature] = ents.size();
     }
+    // dump map
+    dumpEntitiesSizeMap(entMap);
+    // iterate over containers (BFS ou DFS?)
+    for (std::vector<PajeContainer*>::iterator contIt = containers.begin(); contIt != containers.end(); contIt++);
     return 0;
+}
+
+bool dumpEntitiesSizeMap(std::map<PajeTypeNature,int> entMap) {
+    std::map<PajeTypeNature,int>::iterator entMapIt;
+    std::string nature;
+    for (entMapIt = entMap.begin(); entMapIt != entMap.end(); entMapIt++) {
+        switch((*entMapIt).first) {
+            case PAJE_EventType:
+                nature = "PajeEvent";
+                break;
+            case PAJE_LinkType:
+                nature = "PajeLink";
+                break;
+            case PAJE_StateType:
+                nature = "PajeState";
+                break;
+            case PAJE_VariableType:
+                nature = "PajeVariable";
+                break;
+            default:
+                nature = "Unknown";
+        }
+        std::cout << nature << ": " << (*entMapIt).second << std::endl;
+    }
+    return true;
 }
