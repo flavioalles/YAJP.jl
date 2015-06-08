77714d47f0c7df62bad4b37b90767bb305618b59
Flavio Alles <flavio.alles@gmail.com>
2015-06-08 17:27:07 -0300

    Reordering of declarations.

--- src/YarosTrace.h
+++ src/YarosTrace.h
@@ -6,12 +6,11 @@ class YarosTrace : public PajeUnity {
 public:
     YarosTrace(const std::string tracefile);
     ~YarosTrace();
-    std::vector<std::string*>& getTopology();
-    std::vector<PajeContainer*>& getContainersOfDepth(int depth);
-    std::vector<PajeContainer*>& getContainersOfName(std::string name);
-    // Disallowing of compiler generated functions
     YarosTrace() = delete;
     YarosTrace(const YarosTrace&) = delete;
     YarosTrace& operator=(const YarosTrace&) = delete;
+    std::vector<std::string*>& getTopology();
+    std::vector<PajeContainer*>& getContainersOfDepth(int depth);
+    std::vector<PajeContainer*>& getContainersOfName(std::string name);
 };
 #endif
