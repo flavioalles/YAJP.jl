07594a166ae44420fefb918a4b6305699c5f85e0
Flavio Alles <flavio.alles@gmail.com>
2015-07-01 18:57:58 -0300

    Adding src/app.

---
+++ src/app/kmeans.cpp
@@ -0,0 +1,32 @@
+#include <boost/filesystem.hpp>
+#include <dlib/svm/kkmeans.h>
+#include <iostream>
+#include "../lib/YarosUnity.h"
+#include "../lib/YarosContainer.h"
+
+int CONTAINER_DEPTH = 5;
+int NUM_CENTERS_MAX = 10;
+std::string TYPE_NAME = "Worker State";
+std::string STATE_NAME = "chol_model_11";
+
+int main(int argc, char* argv[]) {
+    if (argc != 2) {
+        std::cout << "Wrong usage." << std::endl;
+        return 1;
+    }
+    if (!boost::filesystem::exists(argv[1])) {
+        std::cout << "Inexistent trace file." << std::endl;
+        return 2;
+    }
+    YarosUnity* unity = new YarosUnity(argv[1]);
+    for (auto& c: unity->getContainersOfDepth(CONTAINER_DEPTH)) {
+        for (auto& d: static_cast<YarosContainer*>(c)->getData(unity->entityTypeWithName(TYPE_NAME)))
+            if (d->getName() == STATE_NAME);
+        break;
+    }
+    // dlib::pick_initial_centers();
+    // dlib::find_clusters_using_kmeans();
+    // dlib::nearest_center();
+    delete unity;
+    return 0;
+}
