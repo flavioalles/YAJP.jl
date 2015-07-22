cf9e723475fb5d763ec62738e8fcb12d187d4267
Flavio Alles <flavio.alles@gmail.com>
2015-07-22 14:41:09 -0300

    KMeans app now expects YAML config. file containing containers and data to be acquired from trace.

--- Makefile
+++ Makefile
@@ -15,7 +15,7 @@ explore:
 	install bin/explore ${HOME}/bin
 kmeans:
 	install -d bin
-	clang -std=c++11 -O3 -o bin/kmeans src/app/kmeans.cpp -L${PWD}/lib -lstdc++ -lyaros -lpaje -lboost_system -lboost_filesystem
+	clang -std=c++11 -O3 -o bin/kmeans src/app/kmeans.cpp -L${PWD}/lib -lstdc++ -lyaros -lpaje -lboost_system -lboost_filesystem -lyaml-cpp
 	install -d ${HOME}/bin
 	install bin/kmeans ${HOME}/bin
 plot:
--- src/app/kmeans.cpp
+++ src/app/kmeans.cpp
@@ -2,49 +2,53 @@
 #include <fstream>
 #include <iostream>
 #include <string>
+#include <yaml-cpp/yaml.h>
 #include "../lib/YarosCluster.h"
 #include "../lib/YarosContainer.h"
 #include "../lib/YarosUnity.h"
+#include "../lib/YarosUtils.h"
 
 const int K = 3;
 const std::string OUTPUT_FILE {"kmeans.out"};
 const std::string SEP {","};
-const std::map<std::string,std::list<std::string>> NAMES {
-    {"tasks",{"chol_model_11","chol_model_21","chol_model_22"}},
-    {"misc",{"Callback","FetchingInput","Initializing","Overhead","PushingOutput","Scheduling"}}
-};
-const std::list<std::string> TYPES {"Worker"};
-
-std::string dataInNames(std::string name) {
-    for (auto& d: NAMES)
-        for (auto& n: d.second)
-            if (n == name)
-                return d.first;
-    return std::string();
-}
 
 int main(int argc, char* argv[]) {
-    if ((argc < 2) || (argc > 3) || ((argc == 3) && (argv[1] != std::string("--no-overwrite")))) {
+    if ((argc < 3) || (argc > 4) || ((argc == 4) && (argv[1] != std::string("--no-overwrite")))) {
         std::cout << "Wrong usage." << std::endl;
         return 1;
     }
-    if (((argc == 2) && (!boost::filesystem::is_regular_file(argv[1]))) || ((argc == 3) && (!boost::filesystem::is_regular_file(argv[2])))) {
-        std::cout << "Inexistent trace file." << std::endl;
+    if (((argc == 3) && (!boost::filesystem::is_regular_file(argv[1]))) || ((argc == 4) && (!boost::filesystem::is_regular_file(argv[2])))) {
+        std::cout << "Inexistent config. file." << std::endl;
         return 2;
     }
-    std::cout << "Simulating Paje trace..." << std::endl;
+    if (((argc == 3) && (!YarosUtils::checkConfig(argv[1]))) || ((argc == 4) && (!YarosUtils::checkConfig(argv[2])))) {
+        std::cout << "Inconsistent config. file." << std::endl;
+        return 3;
+    }
+    if (((argc == 3) && (!boost::filesystem::is_regular_file(argv[2]))) || ((argc == 4) && (!boost::filesystem::is_regular_file(argv[3])))) {
+        std::cout << "Inexistent trace file." << std::endl;
+        return 4;
+    }
+    std::cout << "Get clustering config. & simulating Paje trace..." << std::endl;
+    YAML::Node config;
     YarosUnity* unity;
-    if (argc == 2)
-        unity = new YarosUnity(argv[1]);
-    else
+    if (argc == 3) {
+        config = YarosUtils::getConfig(argv[1]);
         unity = new YarosUnity(argv[2]);
+    }
+    else {
+        config = YarosUtils::getConfig(argv[2]);
+        unity = new YarosUnity(argv[3]);
+    }
     std::cout << "Retrieving data..." << std::endl;
     std::map<std::string,std::map<std::string,double>>* cMap = new std::map<std::string,std::map<std::string,double>>();
-    for (auto t: TYPES) {
-        for (auto& c: unity->getContainersOfType(t)) {
+    for (auto t: config["containers"]) {
+        for (auto& c: unity->getContainersOfType(t.as<std::string>())) {
             std::map<std::string,double>* dMap = new std::map<std::string,double>();
             for (auto& d: static_cast<YarosContainer*>(c)->getData()) {
-                std::string mapKey = dataInNames(d->getName());
+                // discover if container name is in config["data"]
+                // if yes, config["data"].first is dMap's key for data in this container
+                std::string mapKey = YarosUtils::dataInConfig(config, d->getName());
                 if (!mapKey.empty()) {
                     if (dMap->find(mapKey) == dMap->end())
                         dMap->insert({mapKey,(d->getEnd()-d->getStart())});
@@ -52,10 +56,10 @@ int main(int argc, char* argv[]) {
                         (dMap->find(mapKey))->second += (d->getEnd()-d->getStart());
                 }
             }
-            // insert NAMES that were not present in YarosContainer->getData()
-            for (auto& d: NAMES)
-                if (dMap->find(d.first) == dMap->end())
-                    dMap->insert({d.first,0.0});
+            // insert names in config["data"] that were not present in YarosContainer->getData()
+            for (auto d: config["data"])
+                if (dMap->find(d.first.as<std::string>()) == dMap->end())
+                    dMap->insert({d.first.as<std::string>(),0.0});
             cMap->insert({c->name(),*dMap});
         }
     }
@@ -64,12 +68,12 @@ int main(int argc, char* argv[]) {
     std::cout << "Writing results to file..." << std::endl;
     std::fstream filestream;
     std::string outputPath;
-    if (argc == 2) {
-        outputPath = (boost::filesystem::path(argv[1]).parent_path()/boost::filesystem::path(OUTPUT_FILE)).string();
-    }
-    else { // (argc == 3)
-        int index = 1;
+    if (argc == 3) {
         outputPath = (boost::filesystem::path(argv[2]).parent_path()/boost::filesystem::path(OUTPUT_FILE)).string();
+    }
+    else {
+        int index = 1;
+        outputPath = (boost::filesystem::path(argv[3]).parent_path()/boost::filesystem::path(OUTPUT_FILE)).string();
         std::string basePath = outputPath;
         while (boost::filesystem::is_regular_file(boost::filesystem::path(outputPath))) {
             outputPath = (boost::filesystem::path(basePath)).replace_extension(boost::filesystem::path(std::to_string(index)+(boost::filesystem::path(OUTPUT_FILE).extension().string()))).string();
@@ -78,15 +82,15 @@ int main(int argc, char* argv[]) {
     }
     filestream.open(outputPath, std::ios::out);
     filestream << "Container" << SEP << "Centroid";
-    // assuming that iteration over std::pair's of std::map follows always the same order
-    for (auto d: NAMES)
+    // assuming that iteration over std::pair's of std::map always follows the same order
+    for (auto d: config["data"])
         filestream << SEP << d.first;
     filestream << std::endl;
     for (auto& c: gMap) {
         filestream << c.first << SEP << c.second;
-        // assuming that iteration over std::pair's of std::map follows always the same order
-        for (auto& d: NAMES)
-            filestream << SEP << (((cMap->find(c.first))->second).find(d.first))->second;
+        // assuming that iteration over std::pair's of std::map follows the same order
+        for (auto d: config["data"])
+            filestream << SEP << (((cMap->find(c.first))->second).find(d.first.as<std::string>()))->second;
         filestream << std::endl;
     }
     filestream.close();
