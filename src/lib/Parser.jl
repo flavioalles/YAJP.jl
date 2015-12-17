a4534f3edcaeb5d654ea37139fb469e0747e0d21
Flavio Alles <flavio.alles@gmail.com>
2015-12-17 17:56:42 -0200

    Adding discard events option in StarPU's parser.

--- src/lib/Parser.jl
+++ src/lib/Parser.jl
@@ -5,11 +5,13 @@ using Constants, Data
 """
 Acquires desired `StarPU` information from csv or wsv dump of Pajé trace.
 
-The expected argument is a path to the csv (or wsv) trace that will be parsed. No checking is done to insure existence and/or readability of file - `SystemError` is thrown and propagated (i.e. not caught here) in case the file cannot be opened.
+The expected argument is a path to the csv (or wsv) trace that will be parsed. No checking is done to insure existence and/or readability of file - `SystemError` is thrown and propagated (i.e. not caught here) in case the file cannot be opened. If file is neither a csv nor a wsv, an `ErrorException` is raised.
+
+An optional argument is (`discard`) tell the parser if the events present in the `STARPU_EVENTS` should be the ones to keep (`discard=false`) or to discard (`discard=true`).
 
 The function returns an object of type `Trace` (exported by the `Data` module).
 """
-function starpu(path::AbstractString)
+function starpu(path::AbstractString, discard=false)
     # build trace (FYI: trace is a reserved word (its a function))
     tr = Trace(Vector{Worker}(), Vector{TasqType}())
     # set field separator Char according to file extension (csv or wsv)
@@ -30,14 +32,26 @@ function starpu(path::AbstractString)
             # build Worker and push it to return array
             wk = Worker(splitline[end], node(string(splitline[end])), parse(Float64, splitline[4]), parse(Float64, splitline[5]), Vector{Tasq}())
             push!(tr.workers, wk)
-        elseif splitline[3] in STARPU_STATES && splitline[8] in STARPU_EVENTS
-            # check if TasqType has been added to Trace
-            tt = TasqType(splitline[8], splitline[9], splitline[11], parse(Int, splitline[12]))
-            !(tt in tr.tasqtypes)? push!(tr.tasqtypes, tt): nothing
-            # build task object and add to worker
-            # assumes that the current task always belongs to the previously added Worker
-            tq = Tasq(splitline[8], parse(Int, splitline[10]), parse(Float64, splitline[4]), parse(Float64, splitline[5]))
-            push!(tr.workers[end].tasqs, tq)
+        elseif splitline[3] in STARPU_STATES
+            if (!discard && splitline[8] in STARPU_EVENTS) || (discard && !(splitline[8] in STARPU_EVENTS))
+                # check if splitline is user-defined event
+                if length(splitline) > 8
+                    tt = TasqType(splitline[8], splitline[9], splitline[11], parse(Int, splitline[12]))
+                else
+                    tt = TasqType(splitline[8], "NA", "NA", zero(Int))
+                end
+                # check if TasqType has been added to Trace
+                !(tt in tr.tasqtypes)? push!(tr.tasqtypes, tt): nothing
+                # build task object and add to worker
+                # assumes that the current task always belongs to the previously added Worker
+                # check if splitline is user-defined event
+                if length(splitline) > 8
+                    tq = Tasq(splitline[8], parse(Int, splitline[10]), parse(Float64, splitline[4]), parse(Float64, splitline[5]))
+                else
+                    tq = Tasq(splitline[8], zero(Int), parse(Float64, splitline[4]), parse(Float64, splitline[5]))
+                end
+                push!(tr.workers[end].tasqs, tq)
+            end
         end
     end
     close(file)
