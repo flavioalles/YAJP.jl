b7d9ed48efde31cc52b50f54be78e0e979ddffd8
Flavio Alles <flavio.alles@gmail.com>
2016-01-10 22:25:48 -0200

    Added metrics dumping.

--- src/app/dump.jl
+++ src/app/dump.jl
@@ -3,18 +3,54 @@
 # TODO: come up with a better solution to import my lib code
 push!(LOAD_PATH, "src/lib")
 
-using Data, Parser
+using Data, Load, Parser
 
 SEP = ","
 
+# TODO: doc
+function checkargs{T<:AbstractString}(args::Vector{T})
+    if length(args) < 2 || length(args) > 3
+        return false
+    elseif !isfile(args[1]) || !isfile(args[2])
+        return false
+    elseif length(args) == 3
+        if !startswith(args[3], "--slices=")
+            return false
+        else
+            try
+                if parse(Int, replace(args[3], "--slices=","")) < 1
+                    return false
+                end
+            catch
+                return false
+            end
+        end
+    end
+    return true
+end
+
+"Dumps to `csv` load balancing metrics. Dump is made in the same dir. as `path` - i.e. trace location."
+function dumpmetrics(tr::Trace, path::AbstractString, slices::Int, sep::AbstractString)
+    location = joinpath(path, "metrics.csv")
+    output = open(location, "w")
+    # generate header
+    write(output, "slice$(sep)std$(sep)skewness$(sep)kurtosis$(sep)pimbalance$(sep)imbalancep$(sep)imbalancet\n")
+    # output slices
+    for (index,metrics) in enumerate(zip(std(tr,slices), skewness(tr,slices), kurtosis(tr,slices), pimbalance(tr,slices), imbalancep(tr,slices), imbalancet(tr,slices)))
+        write(output, "$(index)$(sep)$(metrics[1])$(sep)$(metrics[2])$(sep)$(metrics[3])$(sep)$(metrics[4])$(sep)$(metrics[5])$(sep)$(metrics[6])\n")
+    end
+    close(output)
+    return location
+end
+
 "Dumps to `csv` trace perf. data. Dump is made in the same dir. as `path` - i.e. trace."
-function dumpperf(tr::Trace, path::AbstractString, sep::AbstractString)
+function dumpperf(tr::Trace, path::AbstractString, slices::Int, sep::AbstractString)
     location = joinpath(path, "perf.csv")
     output = open(location, "w")
     # generate header
-    write(output, "span\n")
+    write(output, "span$(sep)began$(sep)ended\n")
     # output span
-    write(output, "$(span(tr))\n")
+    write(output, "$(span(tr))$(sep)$(began(tr))$(sep)$(ended(tr))\n")
     close(output)
     return location
 end
@@ -36,12 +72,24 @@ function dumpevents(tr::Trace, path::AbstractString, sep::AbstractString)
     return location
 end
 
-if length(ARGS) == 2 && isfile(ARGS[1]) && isfile(ARGS[2])
+if checkargs(ARGS)
     print("Acquiring trace data...")
     tr = Parser.parsetrace(ARGS[1], ARGS[2])
     println("done.")
+    print("Dumping load balancing metrics...")
+    if length(ARGS) == 2
+        location = dumpmetrics(tr, dirname(ARGS[1]), 1, SEP)
+    else
+        location = dumpmetrics(tr, dirname(ARGS[1]), parse(Int, replace(ARGS[3], "--slices=","")), SEP)
+    end
+    println("done.")
+    println("Metrics data in $(location).")
     print("Dumping performance data...")
-    location = dumpperf(tr, dirname(ARGS[1]), SEP)
+    if length(ARGS) == 2
+        location = dumpperf(tr, dirname(ARGS[1]), 1, SEP)
+    else
+        location = dumpperf(tr, dirname(ARGS[1]), parse(Int, replace(ARGS[3], "--slices=","")), SEP)
+    end
     println("done.")
     println("Performance data in $(location).")
     print("Dumping events data...")
