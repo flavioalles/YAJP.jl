e1a6bdac80220f7f4d5d62a562b0e0c34131136f
Flavio Alles <flavio.alles@gmail.com>
2016-01-14 14:13:56 -0200

    Adding time information to dumped slices (loads & metrics).

--- src/app/dump.jl
+++ src/app/dump.jl
@@ -14,11 +14,11 @@ function checkargs{T<:AbstractString}(args::Vector{T})
     elseif !isfile(args[1]) || !isfile(args[2])
         return false
     elseif length(args) == 3
-        if !startswith(args[3], "--slices=")
+        if !startswith(args[3], "--timestep=")
             return false
         else
             try
-                if parse(Int, replace(args[3], "--slices=","")) < 1
+                if parse(Float64, replace(args[3], "--timestep=","")) < 1
                     return false
                 end
             catch
@@ -30,20 +30,21 @@ function checkargs{T<:AbstractString}(args::Vector{T})
 end
 
 # TODO: doc
-function dumploads(tr::Trace, path::AbstractString, slices::Int, sep::AbstractString)
+function dumploads{T<:Real}(tr::Trace, path::AbstractString, timestep::T, sep::AbstractString)
     location = joinpath(path, "loads.csv")
     output = open(location, "w")
     # generate header
-    header = "slice$(sep)"
+    header = "slice$(sep)begin$(sep)midpoint$(sep)end$(sep)"
     for ct in tr.containers
         ct != tr.containers[end]? header *= "$(ct.name)$(sep)" : header *= "$(ct.name)\n"
     end
     write(output, header)
     # output slice loads
-    timestep = (ended(tr) - began(tr))/slices
     for (slice,ts) in enumerate(began(tr):timestep:ended(tr))
-        loads = "$(slice)$(sep)"
-        if slice <= slices
+        if ts != ended(tr)
+            ts + timestep < ended(tr)? ed = ts + timestep : ed = ended(tr)
+            midpoint = ts + (ed - ts)/2
+            loads = "$(slice)$(sep)$(ts)$(sep)$(midpoint)$(sep)$(ed)$(sep)"
             for ct in tr.containers
                 if ct != tr.containers[end]
                     loads *= "$(load(ct, ts, ts+timestep))$(sep)"
@@ -59,14 +60,17 @@ function dumploads(tr::Trace, path::AbstractString, slices::Int, sep::AbstractSt
 end
 
 "Dumps to `csv` load balancing metrics. Dump is made in the same dir. as `path` - i.e. trace location."
-function dumpmetrics(tr::Trace, path::AbstractString, slices::Int, sep::AbstractString)
+function dumpmetrics{T<:Real}(tr::Trace, path::AbstractString, timestep::T, sep::AbstractString)
     location = joinpath(path, "metrics.csv")
     output = open(location, "w")
     # generate header
-    write(output, "slice$(sep)std$(sep)skewness$(sep)kurtosis$(sep)pimbalance$(sep)imbalancep$(sep)imbalancet\n")
+    write(output, "slice$(sep)begin$(sep)midpoint$(sep)end$(sep)std$(sep)skewness$(sep)kurtosis$(sep)pimbalance$(sep)imbalancep$(sep)imbalancet\n")
     # output slices
-    for (index,metrics) in enumerate(zip(std(tr,slices), skewness(tr,slices), kurtosis(tr,slices), pimbalance(tr,slices), imbalancep(tr,slices), imbalancet(tr,slices)))
-        write(output, "$(index)$(sep)$(metrics[1])$(sep)$(metrics[2])$(sep)$(metrics[3])$(sep)$(metrics[4])$(sep)$(metrics[5])$(sep)$(metrics[6])\n")
+    for (index,metrics) in enumerate(zip(std(tr,timestep), skewness(tr,timestep), kurtosis(tr,timestep), pimbalance(tr,timestep), imbalancep(tr,timestep), imbalancet(tr,timestep)))
+        bg = began(tr) + timestep*(index-1)
+        bg + timestep <=  ended(tr)? ed = bg + timestep : ed = ended(tr)
+        midpoint = bg +  (ed - bg)/2
+        write(output, "$(index)$(sep)$(bg)$(sep)$(midpoint)$(sep)$(ed)$(sep)$(metrics[1])$(sep)$(metrics[2])$(sep)$(metrics[3])$(sep)$(metrics[4])$(sep)$(metrics[5])$(sep)$(metrics[6])\n")
     end
     close(output)
     return location
@@ -109,7 +113,7 @@ if checkargs(ARGS)
     if length(ARGS) == 2
         location = dumploads(tr, dirname(ARGS[1]), 1, SEP)
     else
-        location = dumploads(tr, dirname(ARGS[1]), parse(Int, replace(ARGS[3], "--slices=","")), SEP)
+        location = dumploads(tr, dirname(ARGS[1]), parse(Float64, replace(ARGS[3], "--timestep=","")), SEP)
     end
     println("done.")
     println("Loads data in $(location).")
@@ -117,7 +121,7 @@ if checkargs(ARGS)
     if length(ARGS) == 2
         location = dumpmetrics(tr, dirname(ARGS[1]), 1, SEP)
     else
-        location = dumpmetrics(tr, dirname(ARGS[1]), parse(Int, replace(ARGS[3], "--slices=","")), SEP)
+        location = dumpmetrics(tr, dirname(ARGS[1]), parse(Float64, replace(ARGS[3], "--timestep=","")), SEP)
     end
     println("done.")
     println("Metrics data in $(location).")
@@ -132,6 +136,6 @@ if checkargs(ARGS)
     exit(0)
 else
     println("Wrong usage.")
-    println("dump.jl <csv-trace> <yaml-config> [--slices=#]")
+    println("dump.jl <csv-trace> <yaml-config> [--timestep=#]")
     exit(1)
 end
