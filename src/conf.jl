78982e45725814c9e60c435a8040280404db76ff
Flavio Alles <flavio.alles@gmail.com>
2016-02-18 16:41:07 -0200

    Modifying conf. check (and, thus, conf. expectations).

--- src/conf.jl
+++ src/conf.jl
@@ -1,10 +1,18 @@
-const FIELDS = ["containers", "states", "keep", "discard"]
+const REQUIRED = ["containers", "states", "keep"]
+const OPTIONAL = ["discard"]
 
 "Checks if YAML config. file represented by `config` is consistent"
 function checkconfig(path::AbstractString)
     config = YAML.load_file(path)
-    for field in FIELDS
+    # check if all REQUIRED fields are present
+    for field in REQUIRED
         if !haskey(config, field)
+            return false
+        end
+    end
+    # check config fields
+    for field in keys(config)
+        if !(field in REQUIRED || field in OPTIONAL)
             return false
         elseif typeof(config[field]) != Array{Any,1}
             return false
