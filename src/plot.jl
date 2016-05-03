24a7dbcf6536fe39ba393bb4db31a993513d65cb
Flavio Alles <flavio.alles@gmail.com>
2016-05-03 18:42:08 -0300

    Adding constants to heat map methods.

--- src/plot.jl
+++ src/plot.jl
@@ -1,9 +1,20 @@
+# integer used as step in xticks def.
+STEPDENOM = 40
+
 """
     heatmap(tr::YAJP.Trace, timestep::Real)
 
 Returns a [Gadfly] plot depicting `tr`s normalized load evolution (at every `timestep`) as a [Gadfly] `rectbin`.
 """
 function heatmap(tr::YAJP.Trace, timestep::Real)
+    # major labels font size
+    MAJORLABEL = 18pt
+    # minor labels font size
+    MINORLABEL = 12pt
+    # key title font size
+    KEYTITLE = 14pt
+    # key label font size
+    KEYLABEL = 14pt
     # get loads
     lds = loads(tr, timestep)
     # sort DataFrame by container name
@@ -29,14 +40,17 @@ function heatmap(tr::YAJP.Trace, timestep::Real)
                Coord.cartesian(xmin=YAJP.began(tr), xmax=YAJP.ended(tr)),
                Guide.xticks(ticks=collect(
                             convert(Int, floor(YAJP.began(tr))):
-                            convert(Int, round(YAJP.span(tr)/20)):
+                            convert(Int, round(YAJP.span(tr)/STEPDENOM)):
                             convert(Int, ceil(YAJP.ended(tr))))),
                Guide.xlabel("Time (s)", orientation=:horizontal),
                Guide.ylabel("Resource", orientation=:vertical),
                Scale.color_continuous(minvalue=0.0, maxvalue=1.0),
-               Guide.title("Normalized Load per Resource at Every $(timestep) s"),
                Guide.colorkey("Load"),
-               Theme(guide_title_position=:center))
+               Theme(guide_title_position=:center,
+                    major_label_font_size=MAJORLABEL,
+                    minor_label_font_size=MINORLABEL,
+                    key_title_font_size=KEYTITLE,
+                    key_label_font_size=KEYLABEL))
 end
 
 """
@@ -45,6 +59,14 @@ end
 Returns a [Gadfly] plot depicting `tr`s evolution for load imbalance metric `f` (at every `timestep`) as a [Gadfly] `rectbin`.
 """
 function heatmap(tr::YAJP.Trace, f::Function, timestep::Real, drop::Int=0)
+    # major labels font size
+    MAJORLABEL = 14pt
+    # minor labels font size
+    MINORLABEL = 12pt
+    # key title font size
+    KEYTITLE = 14pt
+    # key label font size
+    KEYLABEL = 12pt
     # retrieve metric
     mtr = metrics(tr, f, timestep, drop)
     # select appropriate metric y-label and title start
@@ -67,14 +89,17 @@ function heatmap(tr::YAJP.Trace, f::Function, timestep::Real, drop::Int=0)
                                 xmax=YAJP.ended(tr)-drop),
                 Guide.xticks(ticks=collect(
                                 convert(Int, floor(YAJP.began(tr)+drop)):
-                                convert(Int, round(YAJP.span(tr)/20)):
+                                convert(Int, round(YAJP.span(tr)/STEPDENOM)):
                                 convert(Int, ceil(YAJP.ended(tr)-drop)))),
                 Guide.xlabel("Time (s)", orientation=:horizontal),
                 Scale.y_discrete(labels= x->""),
-                Guide.ylabel(nothing),
+                Guide.ylabel(text, orientation=:vertical),
                 Scale.color_continuous(minvalue=minimum(mtr[:value]),
                                        maxvalue=maximum(mtr[:value])),
-                Guide.title("$(text) Evolution at Every $(timestep) s"),
                 Guide.colorkey("Value"),
-                Theme(guide_title_position=:left))
+                Theme(guide_title_position=:center,
+                     major_label_font_size=MAJORLABEL,
+                     minor_label_font_size=MINORLABEL,
+                     key_title_font_size=KEYTITLE,
+                     key_label_font_size=KEYLABEL))
 end
