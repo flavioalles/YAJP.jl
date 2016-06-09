41029fdf1b22979c9bc40f916e877b75293407e2
Flavio Alles <flavio.alles@gmail.com>
2016-06-09 20:19:08 -0300

    Changing metrics plot method.

--- src/plot.jl
+++ src/plot.jl
@@ -54,53 +54,67 @@ function loadplot(tr::YAJP.Trace, timestep::Real)
 end
 
 """
-    heatmap(tr::YAJP.Trace, f::Function, timestep::Real, drop::Int=0)
+    metricsplot(tr::YAJP.Trace, f::Function, timestep::Real, drop::Int=0)
 
 Returns a [Gadfly] plot depicting `tr`s evolution for load imbalance metric `f` (at every `timestep`) as a [Gadfly] `rectbin`.
 """
-function heatmap(tr::YAJP.Trace, f::Function, timestep::Real, drop::Int=0)
+function metricsplot(tr::YAJP.Trace, timestep::Real, drop::Int=0)
     # major labels font size
     MAJORLABEL = 14pt
     # minor labels font size
     MINORLABEL = 12pt
-    # key title font size
-    KEYTITLE = 14pt
-    # key label font size
-    KEYLABEL = 12pt
-    # retrieve metric
-    mtr = metrics(tr, f, timestep, drop)
-    # select appropriate metric y-label and title start
-    if f == std
-        text = "Standard Deviation (s)"
-    elseif f == skewness
-        text = "Skewness"
-    elseif f == kurtosis
-        text = "Kurtosis"
-    elseif f == pimbalance
-        text = "Percent Imbalance"
-    elseif f == imbalancep
-        text = "Imbalance Percentage"
-    else
-        text = "Imbalance Time (s)"
+    # plots container
+    plots = Plot[]
+    for f in [pimbalance,
+              imbalancep,
+              imbalancet,
+              std,
+              skewness,
+              kurtosis]
+        # retrieve metric
+        mtr = metrics(tr, f, timestep, drop)
+        # select appropriate metric  y-label
+        # and if x-label and ticks label should exist
+        if f == pimbalance
+            verticallabel = "Percent Imbalance"
+            horizontallabel = nothing
+            tickslabeled = false
+        elseif f == imbalancep
+            verticallabel = "Imbalance Percentage"
+            horizontallabel = nothing
+            tickslabeled = false
+        elseif f == imbalancet
+            verticallabel = "Imbalance Time (s)"
+            horizontallabel = nothing
+            tickslabeled = false
+        elseif f == std
+            verticallabel = "Standard Deviation (s)"
+            horizontallabel = nothing
+            tickslabeled = false
+        elseif f == skewness
+            verticallabel = "Skewness"
+            horizontallabel = nothing
+            tickslabeled = false
+        else
+            verticallabel = "Kurtosis"
+            horizontallabel = "Time (s)"
+            tickslabeled = true
+        end
+        # plot metric heat map
+        push!(plots, plot(mtr[!isnan(mtr[:value]), :],
+                     x="midpoint", y="value", Geom.line,
+                     Coord.cartesian(xmin=YAJP.began(tr)+drop,
+                                     xmax=YAJP.ended(tr)-drop),
+                     Guide.xticks(ticks=collect(
+                                     convert(Int, floor(YAJP.began(tr)+drop)):
+                                     convert(Int, round(YAJP.span(tr)/STEPDENOM)):
+                                     convert(Int, ceil(YAJP.ended(tr)-drop))),
+                                  label=tickslabeled,
+                                  orientation=:horizontal),
+                     Guide.xlabel(horizontallabel, orientation=:horizontal),
+                     Guide.ylabel(verticallabel, orientation=:vertical),
+                     Theme(major_label_font_size=MAJORLABEL,
+                           minor_label_font_size=MINORLABEL)))
     end
-    # plot metric heat map
-    return plot(mtr[!isnan(mtr[:value]), :],
-                x="midpoint", y="metric", color="value", Geom.rectbin,
-                Coord.cartesian(xmin=YAJP.began(tr)+drop,
-                                xmax=YAJP.ended(tr)-drop),
-                Guide.xticks(ticks=collect(
-                                convert(Int, floor(YAJP.began(tr)+drop)):
-                                convert(Int, round(YAJP.span(tr)/STEPDENOM)):
-                                convert(Int, ceil(YAJP.ended(tr)-drop)))),
-                Guide.xlabel("Time (s)", orientation=:horizontal),
-                Scale.y_discrete(labels= x->""),
-                Guide.ylabel(text, orientation=:vertical),
-                Scale.color_continuous(minvalue=minimum(mtr[:value]),
-                                       maxvalue=maximum(mtr[:value])),
-                Guide.colorkey("Value"),
-                Theme(guide_title_position=:center,
-                     major_label_font_size=MAJORLABEL,
-                     minor_label_font_size=MINORLABEL,
-                     key_title_font_size=KEYTITLE,
-                     key_label_font_size=KEYLABEL))
+    return vstack(plots)
 end
