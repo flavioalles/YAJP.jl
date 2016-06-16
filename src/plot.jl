78db33d6342e864d7c006482277e668957790c8f
Flavio Alles <flavio.alles@gmail.com>
2016-06-16 12:06:05 -0300

    Load plots now accepts multiple time steps.

--- src/plot.jl
+++ src/plot.jl
@@ -2,11 +2,11 @@
 STEPDENOM = 30
 
 """
-    loadplot(tr::YAJP.Trace, timestep::Real)
+    loadplot(tr::YAJP.Trace, timestep::Real...)
 
-Returns a [Gadfly] plot depicting `tr`s normalized load evolution (at every `timestep`) as a [Gadfly] `rectbin`.
+Returns a container of [Gadfly] plots depicting `tr`s normalized load evolution (at every `timestep`) as a [Gadfly] `rectbin`s.
 """
-function loadplot(tr::YAJP.Trace, timestep::Real)
+function loadplot(tr::YAJP.Trace, timestep::Real...)
     # major labels font size
     MAJORLABEL = 18pt
     # minor labels font size
@@ -15,42 +15,58 @@ function loadplot(tr::YAJP.Trace, timestep::Real)
     KEYTITLE = 14pt
     # key label font size
     KEYLABEL = 14pt
-    # get loads
-    lds = loads(tr, timestep)
-    # sort DataFrame by container name
-    try
-        # MPI
-        lds = sort!(lds,
-                    cols = order(:container,
-                                by = x -> parse(Int, replace(x, "rank", ""))),
-                    rev = true)
-    catch e
-        if isa(e, ArgumentError)
-            # StarPU
+    # plots container
+    plots = Plot[]
+    for (id,ts) in enumerate(timestep)
+        # get loads
+        lds = loads(tr, ts)
+        # sort DataFrame by container name
+        try
+            # MPI
             lds = sort!(lds,
-                    cols = order(:container,
-                                by = x -> parse(Int, replace(x, "CPU", ""))),
-                    rev = true)
-        else
-            throw(e)
+                        cols = order(:container,
+                                    by = x -> parse(Int, replace(x, "rank", ""))),
+                        rev = true)
+        catch e
+            if isa(e, ArgumentError)
+                # StarPU
+                lds = sort!(lds,
+                        cols = order(:container,
+                                    by = x -> parse(Int, replace(x, "CPU", ""))),
+                        rev = true)
+            else
+                throw(e)
+            end
         end
+        if id != length(timestep)
+            horizontallabel = nothing
+            tickslabel = false
+        else
+            horizontallabel = "Time (s)"
+            tickslabel = true
+        end
+        # plot heat map of load evolution per resource
+        push!(plots,
+                   plot(lds, x="midpoint", y="container", color="normalized",
+                   Geom.rectbin,
+                   Coord.cartesian(xmin=YAJP.began(tr), xmax=YAJP.ended(tr)),
+                   Guide.xticks(ticks=collect(
+                                    convert(Int, floor(YAJP.began(tr))):
+                                    convert(Int, round(YAJP.span(tr)/STEPDENOM)):
+                                    convert(Int, ceil(YAJP.ended(tr)))),
+                                label=tickslabel,
+                                orientation=:horizontal),
+                   Guide.xlabel(horizontallabel, orientation=:horizontal),
+                   Guide.ylabel("Resource", orientation=:vertical),
+                   Scale.color_continuous(minvalue=0.0, maxvalue=1.0),
+                   Guide.colorkey("Load"),
+                   Theme(guide_title_position=:center,
+                        major_label_font_size=MAJORLABEL,
+                        minor_label_font_size=MINORLABEL,
+                        key_title_font_size=KEYTITLE,
+                        key_label_font_size=KEYLABEL)))
     end
-    # return heat map of load evolution per resource
-    return plot(lds, x="midpoint", y="container", color="normalized", Geom.rectbin,
-               Coord.cartesian(xmin=YAJP.began(tr), xmax=YAJP.ended(tr)),
-               Guide.xticks(ticks=collect(
-                            convert(Int, floor(YAJP.began(tr))):
-                            convert(Int, round(YAJP.span(tr)/STEPDENOM)):
-                            convert(Int, ceil(YAJP.ended(tr))))),
-               Guide.xlabel("Time (s)", orientation=:horizontal),
-               Guide.ylabel("Resource", orientation=:vertical),
-               Scale.color_continuous(minvalue=0.0, maxvalue=1.0),
-               Guide.colorkey("Load"),
-               Theme(guide_title_position=:center,
-                    major_label_font_size=MAJORLABEL,
-                    minor_label_font_size=MINORLABEL,
-                    key_title_font_size=KEYTITLE,
-                    key_label_font_size=KEYLABEL))
+    return plots
 end
 
 """
