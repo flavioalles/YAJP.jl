08ff71b1bdd0f523a751dd787eba4b37c756e0e0
Flavio Alles <flavio.alles@gmail.com>
2016-07-29 16:43:02 -0300

    Load plot only accepts one timestep.

--- src/plot.jl
+++ src/plot.jl
@@ -2,80 +2,60 @@
 STEPDENOM = 30
 
 """
-    loadplot(tr::YAJP.Trace, timestep::Real...)
+    loadplot(tr::YAJP.Trace, timestep::Real)
 
-Returns a container of [Gadfly] plots depicting `tr`s normalized load evolution (at every `timestep`) as a [Gadfly] `rectbin`s.
+Returns a [Gadfly](https://github.com/dcjones/Gadfly.jl) plot depicting `tr`s normalized load evolution (at every `timestep`) as a [`rectbin`](http://dcjones.github.io/Gadfly.jl/geom_rectbin.html).
 """
-function loadplot(tr::YAJP.Trace, timestep::Real...)
-    if length(timestep) == 1
-        # major labels font size
-        MAJORLABEL = 18pt
-        # minor labels font size
-        MINORLABEL = 12pt
-        # key title font size
-        KEYTITLE = 14pt
-        # key label font size
-        KEYLABEL = 14pt
-    else
-        # major labels font size
-        MAJORLABEL = 12pt
-        # minor labels font size
-        MINORLABEL = 8pt
-        # key title font size
-        KEYTITLE = 12pt
-        # key label font size
-        KEYLABEL = 12pt
-    end
-    # plots container
-    plots = Plot[]
-    for (id,ts) in enumerate(timestep)
-        # get loads
-        lds = loads(tr, ts)
-        # sort DataFrame by container name
-        try
-            # MPI
+function loadplot(tr::YAJP.Trace, timestep::Real)
+    # major labels font size
+    MAJORLABEL = 18pt
+    # minor labels font size
+    MINORLABEL = 12pt
+    # key title font size
+    KEYTITLE = 14pt
+    # key label font size
+    KEYLABEL = 14pt
+    # get loads
+    lds = loads(tr, timestep)
+    # sort DataFrame by container name
+    try
+        # MPI
+        lds = sort!(lds,
+                    cols = order(:container,
+                                by = x -> parse(Int, replace(x, "rank", ""))),
+                    rev = true)
+    catch e
+        if isa(e, ArgumentError)
+            # StarPU
             lds = sort!(lds,
-                        cols = order(:container,
-                                    by = x -> parse(Int, replace(x, "rank", ""))),
-                        rev = true)
-        catch e
-            if isa(e, ArgumentError)
-                # StarPU
-                lds = sort!(lds,
-                        cols = order(:container,
-                                    by = x -> parse(Int, replace(x, "CPU", ""))),
-                        rev = true)
-            else
-                throw(e)
-            end
-        end
-        if id != length(timestep)
-            horizontallabel = nothing
+                    cols = order(:container,
+                                by = x -> parse(Int, replace(x, "CPU", ""))),
+                    rev = true)
         else
-            horizontallabel = "Time (s)"
+            throw(e)
         end
-        # plot heat map of load evolution per resource
-        push!(plots,
-                   plot(lds, x="midpoint", y="container", color="normalized",
-                   Geom.rectbin,
-                   Coord.cartesian(xmin=YAJP.began(tr), xmax=YAJP.ended(tr)),
-                   Guide.xticks(ticks=collect(
-                                    convert(Int, floor(YAJP.began(tr))):
-                                    convert(Int, round(YAJP.span(tr)/STEPDENOM)):
-                                    convert(Int, ceil(YAJP.ended(tr)))),
-                                label=true,
-                                orientation=:horizontal),
-                   Guide.xlabel(horizontallabel, orientation=:horizontal),
-                   Guide.ylabel("Resource", orientation=:vertical),
-                   Scale.color_continuous(minvalue=0.0, maxvalue=1.0),
-                   Guide.colorkey("Load"),
-                   Theme(guide_title_position=:center,
-                        major_label_font_size=MAJORLABEL,
-                        minor_label_font_size=MINORLABEL,
-                        key_title_font_size=KEYTITLE,
-                        key_label_font_size=KEYLABEL)))
     end
-    return plots
+    horizontallabel = "Time (s)"
+    # plot heat map of load evolution per resource
+    pl = plot(lds, x="midpoint", y="container", color="normalized",
+              Geom.rectbin,
+              Coord.cartesian(xmin=YAJP.began(tr), xmax=YAJP.ended(tr)),
+              Guide.xticks(ticks=collect(
+                            convert(Int, floor(YAJP.began(tr))):
+                            convert(Int, round(YAJP.span(tr)/STEPDENOM)):
+                            convert(Int, ceil(YAJP.ended(tr)))),
+                           label=true,
+                           orientation=:horizontal),
+              Guide.xlabel(horizontallabel, orientation=:horizontal),
+              Guide.ylabel("Resource", orientation=:vertical),
+              Scale.color_continuous(minvalue=0.0, maxvalue=1.0),
+              Guide.colorkey("Load"),
+              Theme(guide_title_position=:center,
+                    major_label_font_size=MAJORLABEL,
+                    minor_label_font_size=MINORLABEL,
+                    key_title_font_size=KEYTITLE,
+                    key_label_font_size=KEYLABEL))
+    return pl
 end
 
 """
