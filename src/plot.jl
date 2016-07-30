a2f1b80f1da049cb2f191a2f75879638cb1c0b4a
Flavio Alles <flavio.alles@gmail.com>
2016-07-30 17:35:02 -0300

    Adding merged metrics plot method.

--- src/plot.jl
+++ src/plot.jl
@@ -163,3 +163,90 @@ function metricsplot(tr::YAJP.Trace, timestep::Real, norm::Bool=false, individua
     end
     return plots
 end
+
+"""
+    mergedmetricsplot(tr::YAJP.Trace, timestep::Real)
+
+Returns an array of [Gadfly](https://github.com/dcjones/Gadfly.jl) plots depicting `tr`s evolution for each load imbalance metric (at every `timestep`). Plots are merged, mmeaning, severity and shape metrics are plotted together. Severity metrics are normalized.
+"""
+function mergedmetricsplot(tr::YAJP.Trace, timestep::Real)
+    # line width
+    LINEWIDTH = 3pt
+    # point size
+    POINTSIZE = 3pt
+    # key title font size
+    KEYTITLE = 12pt
+    # key label font size
+    KEYLABEL = 10pt
+    # major labels font size
+    MAJORLABEL = 14pt
+    # minor labels font size
+    MINORLABEL = 12pt
+    # plots container
+    plots = Plot[]
+    # retrieve metrics
+    mtr = stack(metrics(tr, timestep, true),
+                [:pimbalance, :imbalancep, :imbalancet,
+                :std, :skewness, :kurtosis])
+    # severity measures
+    push!(plots, plot(mtr[(mtr[:variable] .== :pimbalance) | 
+                          (mtr[:variable] .== :imbalancep) | 
+                          (mtr[:variable] .== :imbalancet) | 
+                          (mtr[:variable] .== :std) & 
+                          (!isnan(mtr[:value])), :],
+                     layer(x="midpoint", y="value", color="variable",
+                           Geom.line, order=1),
+                     layer(x="midpoint", y="value", color="variable",
+                           Geom.point, order=2),
+                     Coord.cartesian(xmin=YAJP.began(tr),
+                                     xmax=YAJP.ended(tr),
+                                     ymin=0.0,
+                                     ymax=1.0),
+                     Guide.xticks(ticks=collect(
+                                     convert(Int, floor(YAJP.began(tr))):
+                                     convert(Int, round(YAJP.span(tr)/STEPDENOM)):
+                                     convert(Int, ceil(YAJP.ended(tr)))),
+                                  label=true,
+                                  orientation=:horizontal),
+                     Guide.xlabel("Time (s)", orientation=:horizontal),
+                     Guide.ylabel(nothing, orientation=:vertical),
+                     Guide.colorkey("Metric"),
+                     Theme(line_width=LINEWIDTH,
+                           default_point_size=POINTSIZE,
+                           key_position=:right,
+                           guide_title_position=:center,
+                           key_title_font_size=KEYTITLE,
+                           key_label_font_size=KEYLABEL,
+                           colorkey_swatch_shape=:circle,
+                           major_label_font_size=MAJORLABEL,
+                           minor_label_font_size=MINORLABEL)))
+    # shape measures
+    push!(plots, plot(mtr[(mtr[:variable] .== :skewness) | 
+                          (mtr[:variable] .== :kurtosis) & 
+                          (!isnan(mtr[:value])), :],
+                     layer(x="midpoint", y="value", color="variable",
+                           Geom.line, order=1),
+                     layer(x="midpoint", y="value", color="variable",
+                           Geom.point, order=2),
+                     Coord.cartesian(xmin=YAJP.began(tr),
+                                     xmax=YAJP.ended(tr)),
+                     Guide.xticks(ticks=collect(
+                                     convert(Int, floor(YAJP.began(tr))):
+                                     convert(Int, round(YAJP.span(tr)/STEPDENOM)):
+                                     convert(Int, ceil(YAJP.ended(tr)))),
+                                  label=true,
+                                  orientation=:horizontal),
+                     Guide.xlabel("Time (s)", orientation=:horizontal),
+                     Guide.ylabel(nothing, orientation=:vertical),
+                     Guide.colorkey("Metric"),
+                     Theme(line_width=LINEWIDTH,
+                           default_point_size=POINTSIZE,
+                           key_position=:right,
+                           guide_title_position=:center,
+                           key_title_font_size=KEYTITLE,
+                           key_label_font_size=KEYLABEL,
+                           colorkey_swatch_shape=:circle,
+                           major_label_font_size=MAJORLABEL,
+                           minor_label_font_size=MINORLABEL)))
+    return plots
+end
